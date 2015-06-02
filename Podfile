ec871d00a32da921332b4c266ff9afee3a16d2df
kyaw Mong <ekyawthan@gmail.com>
2015-06-02 11:08:45 -0400

    add requeirements

---
+++ Podfile
@@ -0,0 +1,12 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.2'
+
+pod 'SwiftyJSON', '~> 2.2.0'
+pod 'MaterialKit'
+pod 'SwiftyJSON', '~> 2.2.0'
+pod 'SwiftDate'
+pod 'Alamofire', '~> 1.2'
+pod 'SnapKit', '~> 0.10.0'
+pod 'SwiftDate'
+pod 'BubbleTransition', '~> 0.1'
+use_frameworks!
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,44 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 2
+#define COCOAPODS_VERSION_PATCH_Alamofire 2
+
+// BubbleTransition
+#define COCOAPODS_POD_AVAILABLE_BubbleTransition
+#define COCOAPODS_VERSION_MAJOR_BubbleTransition 0
+#define COCOAPODS_VERSION_MINOR_BubbleTransition 1
+#define COCOAPODS_VERSION_PATCH_BubbleTransition 3
+
+// MaterialKit
+#define COCOAPODS_POD_AVAILABLE_MaterialKit
+#define COCOAPODS_VERSION_MAJOR_MaterialKit 0
+#define COCOAPODS_VERSION_MINOR_MaterialKit 3
+#define COCOAPODS_VERSION_PATCH_MaterialKit 5
+
+// SnapKit
+#define COCOAPODS_POD_AVAILABLE_SnapKit
+#define COCOAPODS_VERSION_MAJOR_SnapKit 0
+#define COCOAPODS_VERSION_MINOR_SnapKit 10
+#define COCOAPODS_VERSION_PATCH_SnapKit 0
+
+// SwiftDate
+#define COCOAPODS_POD_AVAILABLE_SwiftDate
+#define COCOAPODS_VERSION_MAJOR_SwiftDate 1
+#define COCOAPODS_VERSION_MINOR_SwiftDate 0
+#define COCOAPODS_VERSION_PATCH_SwiftDate 8
+
+// SwiftyJSON
+#define COCOAPODS_POD_AVAILABLE_SwiftyJSON
+#define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_MINOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_PATCH_SwiftyJSON 0
+
